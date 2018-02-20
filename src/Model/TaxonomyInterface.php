7cfc1977b365bd5e005bb4748cc47df16005ad84
Cody Scherer <k0bl@outlook.com>
2018-02-20 11:51:53 -0800

    taxonomy record interface

---
+++ src/Model/TaxonomyInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+interface TaxonomyInterface
+{
+    const TAXONOMY_CODE = 0;
+    const TAXONOMY_GROUPING = 1;
+    const TAXONOMY_CLASSIFICATION = 2;
+    const TAXONOMY_SPECIALIZATION = 3;
+    const TAXONOMY_DEFINITION = 4;
+}
