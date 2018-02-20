322d325d4dae8b4b1ba61e51820ed22d9852c2e1
Cody Scherer <k0bl@outlook.com>
2018-02-20 11:49:24 -0800

    added number entity for phone and fax numbers

---
+++ src/Entity/Number.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="provider_numbers"),
+ * @ORM\HasLifecycleCallbacks
+ */
+class Number
+{
+    /**
+     *@ORM\Column(type="integer")
+     *@ORM\Id
+     *@ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+     /**
+     * @ORM\ManyToOne(targetEntity="Provider", inversedBy="numbers")
+     * @ORM\JoinColumn(name="provider_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    public $provider;
+
+    /**
+     * @ORM\Column()
+     */
+    public $type;
+    
+    /**
+     * @ORM\Column()
+     */
+    public $number;
+
+}
